59d3c95da909d40292d9b1f993be8be28a9beb1a
TalelMejri <talelmejri8@gmail.com>
2024-08-28 23:11:57 +0100

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueTypewriterEffect :strings="['hello', 'world']" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueTypewriterEffect from "vue-typewriter-effect";
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+    }
+  },
   components: {
-    HelloWorld
+    VueTypewriterEffect
   }
 }
 </script>
+
+
+<style>
+
+</style>
